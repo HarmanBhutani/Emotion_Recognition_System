1112c48494552b68fd350339d218c9ccc2832e1a
Harman Bhutani <byteharman0101@gmail.com>
2018-07-08 22:59:35 +0530

    new function added

--- src/face_detect.py
+++ src/face_detect.py
@@ -9,12 +9,11 @@ def crop_face(image, rescale_factor=2):
     imageScaled = cv2.resize(image, (image.shape[0] / rescale_factor,
                                      image.shape[1] / rescale_factor))
 
-    # The image might already be equalized, so no need for that here
+
     gray = cv2.equalizeHist(imageScaled)
     rects = cascade.detectMultiScale(gray, 1.1, 3)
 
-    # We need to find exactly one face in the picture
-    # print("len(rects)")
+   
     print(len(rects))
     if len(rects) is not 1:
         return None
@@ -37,20 +36,19 @@ else:
 while ret:
     ret, frames = capture.read()
 
-    # convert to gray scale of each frames
     gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
 
     faces = classifier.detectMultiScale(gray, 1.1, 1)
     for (x, y, w, h) in faces:
         cv2.rectangle(frames, (x, y), (x + w, y + h), (255, 255, 0), 1)
 
-    # Display frames in a window
+
     frames = cv2.resize(frames, (960, 540))
     cv2.imshow('video', frames)
 
-    # Wait for Esc key to stop
+   
     if cv2.waitKey(33) == 27:
         break
 
-# De-allocate any associated memory usage
+
 cv2.destroyAllWindows()
